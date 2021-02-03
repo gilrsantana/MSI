ead2489c29748a2fdf0029135d1c738dc5fb8da5
Gilmar Santana <g.r.santana@hotmail.com>
2021-02-03 18:21:13 -0300

    Formating horizontal and vertical nav

---
+++ script.js
@@ -0,0 +1,29 @@
+/*const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+
+    burger.addEventListener("click",()=>{
+        nav.classList.toggle("nav-active");
+    })
+}
+
+navSlide();
+*/
+
+var burger = document.querySelector(".burger");
+var menu = document.querySelector(".menu");
+var navLinks = document.querySelector(".nav-links-ver");
+
+navLinks.style.transform = "translateX(101%)";
+
+menu.onclick = function () {
+    if (navLinks.style.transform == "translateX(101%)") {
+        navLinks.style.transform = "translateX(0)";
+        menu.src = "./Img/close.png";
+    } else {
+        navLinks.style.transform = "translateX(101%)";
+        menu.src = "./Img/menu.png";
+    }
+};
+
+
